92478b1a1a661b02d50cf39a59f1e88f2db4f26d
Simone Lusenti <s@bugpilot.io>
2023-10-11 14:40:55 +0200

    add type devs
    Signed-off-by: Simone Lusenti <s@bugpilot.io>

---
+++ bugpilot-next/types/imported_types.d.ts
@@ -0,0 +1,92 @@
+export type Metadata = {
+  triggerType: "wss" | "widget" | "sdk" | "autopilot";
+  triggerSource: never;
+  userProvidedDescription: string;
+  errorInfo: {
+    isErrorNew: boolean;
+    isUserNew: boolean;
+    numOccurences: number;
+    numAffectedUsers: number;
+    originalReportId: string;
+  };
+  [key: string]: any;
+};
+
+export type WorkspaceSettings = {
+  id: string;
+  flags: Flags;
+  overQuota: boolean;
+  sampleRecordingAllUrls?: [];
+  sampleRecordingPercentage: number;
+  sampleRecordingAll: "manual" | "auto";
+  apiWebsocketMode: "manual" | "always-on";
+  enableUserWidgetV2: boolean;
+  userWidgetPosition: "left" | "bottom";
+  showWidgetBranding: boolean;
+  shouldRequireUserDescription: boolean;
+  widgetSettings: {
+    customCSS: string;
+    idleText: string;
+    inProgressText: string;
+    successText: string;
+    elementSelector?: string;
+  };
+  recordLocalStorage: "all" | "off" | "custom";
+  recordLocalStorageFields: string[];
+  recordSessionStorage: "all" | "off" | "custom";
+  recordSessionStorageFields: string[];
+  recordCookies: "all" | "off" | "custom";
+  recordCookiesFields: string[];
+  shouldRecordUUIDs?: boolean;
+  piiRemovalMode?: "manual" | "auto";
+  helpdeskRecordingMode: "manual" | "auto";
+};
+
+export type User = {
+  id?: string | null;
+  email?: string | null;
+  [key: string]: any;
+};
+
+export type ReportData = {
+  id: string;
+  v: string;
+  companyId: string;
+  user: User | null;
+  metadata: Partial<Metadata>;
+  mode: "session" | "recording";
+  sessionId: string | null;
+  sessionToken: string;
+  scriptVersion: string | undefined;
+  url: string;
+  pageTitle: string;
+  userAgent: string;
+  language: string;
+  source: string;
+  sourceInfo: Record<string, unknown>;
+  localStorage: Record<string, unknown>;
+  sessionStorage: Record<string, unknown>;
+  cookies: Record<string, unknown>;
+  screen: {
+    width: number;
+    height: number;
+  };
+  window: {
+    width: number;
+    height: number;
+  };
+  timeZone: string;
+  startedAt: number;
+  chatUserId: string;
+  timeOrigin: number | undefined;
+  timing: Record<string, unknown> | undefined;
+  firstPaint: PerformanceEntry | null;
+  firstContentfulPaint: PerformanceEntry | null;
+  connection: {
+    effectiveType: string;
+    downlink: number;
+    rtt: number;
+  } | null;
+  autopilotErrors?: AutopilotError[];
+  reportGroupId?: string;
+};
---
+++ bugpilot-next/types/index.d.ts
@@ -0,0 +1,36 @@
+import React, { Context } from "react";
+import {
+  Metadata as MetadataBase,
+  ReportData as ReportDataBase,
+  User as UserBase,
+} from "./imported_types";
+
+declare namespace bugpilot {
+  interface Metadata extends MetadataBase {
+    triggerType?: "sdk" | "autopilot" | "wss" | "helpdesk" | "widget" | string;
+    triggerSource?: string;
+    userProvidedDescription?: string;
+    [key: string]: any;
+  }
+
+  interface ReportData extends ReportDataBase {}
+  interface User extends UserBase {}
+
+  interface BugpilotProviderProps {
+    workspaceId: string;
+    user?: User;
+  }
+
+  export const Bugpilot: Context<BugpilotProviderProps>;
+
+  export const useBugpilot: () => {
+    saveBugReport: (
+      metadata?: Metadata,
+      reportDataOverride?: ReportData
+    ) => void;
+    identify: (user: User) => void;
+    logout: () => void;
+  };
+}
+
+export = bugpilot;
